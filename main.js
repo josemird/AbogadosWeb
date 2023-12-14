e22ddac8ed0d904cd99eabfa41dbbf7d2a5e920a
José Miguel <33165751+josemird@users.noreply.github.com>
2023-12-15 00:12:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ document.getElementById("menuButton").addEventListener("click", function() {
     var menu = document.getElementById("menu");
     menu.style.display = (menu.style.display === "none" || menu.style.display === "") ? "block" : "none";
   });
+
+ document.addEventListener("DOMContentLoaded", function() {
+      var video = document.getElementById("myVideo");
+      video.play();
+    });
